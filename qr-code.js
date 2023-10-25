0c298fa7e5a20e4bf3c2f67e260464b459394375
KALIDUWA OFC ✔️ <132440655+MR-KALINDU@users.noreply.github.com>
2023-10-25 18:34:56 +0530

    Create qr-code.js

---
+++ qr-code.js
@@ -0,0 +1,79 @@
+const fs   = require("fs-extra");
+const pino = require("pino");
+let qrcode = require("qrcode-terminal");
+
+
+if (fs.existsSync('./auth_info_baileys')) {
+  fs.emptyDirSync(__dirname + '/auth_info_baileys');
+  require('child_process').exec('rm -rf auth_info_baileys')
+  console.log('\nPlease Wait... Removing Cache files');
+  setTimeout(() => {   console.log(' ')    }, 100);
+  setTimeout(() => {   console.log('Q')    }, 300);
+  setTimeout(() => {   console.log('R')    }, 500);
+  setTimeout(() => {   console.log(' ')    }, 700);
+  setTimeout(() => {   console.log('B')    }, 900);
+  setTimeout(() => {   console.log('Y')    }, 1100);
+  setTimeout(() => {   console.log(' ')    }, 1300);
+  setTimeout(() => {   console.log('S')    }, 1500);
+  setTimeout(() => {   console.log('U')    }, 1700);
+  setTimeout(() => {   console.log('H')    }, 1900);
+  setTimeout(() => {   console.log('A')    }, 2100);
+  setTimeout(() => {   console.log('I')    }, 2300);
+  setTimeout(() => {   console.log('L')    }, 2500);
+  setTimeout(() => {   console.log(' ')    }, 2700);
+  setTimeout(() => {   console.log('Cache Cleared..!\nRun The Script Again')    }, 2900);
+  setTimeout(() => {   process.exit()      }, 3000)
+};
+setTimeout(() => {
+  const { default: makeWASocket, useMultiFileAuthState, Browsers, delay, makeInMemoryStore, } = require("@sampandey001/baileys");
+  const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+  async function sᴜʜᴀɪʟ_ʙᴀɪʟᴇʏs_ǫʀ() {
+    const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
+    try {
+      let session = makeWASocket({ printQRInTerminal: true, logger: pino({ level: "silent" }), browser: Browsers.macOS("Desktop"), auth: state });
+      session.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect, qr } = s;
+        if (connection == "open")
+        {
+          await delay(500);
+          let user = session.user.id;         // User = Number of that user who just Scanned the Qr
+
+//===========================================================================================
+//===============================  SESSION ID   =============================================
+//===========================================================================================
+          let unique = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json') //GETTING CREDS FROM CREDS.json TO GENERATE SESSION ID 
+          c = Buffer.from(unique).toString('base64'); // CHANGE 'base64' ACCORDING TO YOUR NEEDS 
+          console.log(`
+====================  SESSION ID  ===========================                   
+SESSION-ID ==> ${c}\n\n
+Don't provide your SESSION_ID to anyone otherwise that user can access your account.
+Thanks.
+-------------------  SESSION CLOSED   -----------------------
+`)   
+
+let cc = `╔════◇
+║『 *THANKS FOR CHOOSING XLICON-MD* 』
+║ _You complete first step to making Bot._
+╚════════════════════════╝
+╔═════◇
+║  『•••Xlicon_session•••』
+║📤 *1.Github:* [ github.com/salmanytofficial/XLICON-MD ]
+║▶ *2.Ytube:* [ youtube.com/infinite9452 ]
+║⭕ *3.Owner:* [ https://wa.me/8801853262586 ]
+ |📍 *4.Support:* [ https://chat.whatsapp.com/GqLQLnl0tHr2ZHV4UcEl5g ]
+║ *Note :*_Don't provide your SESSION_ID to_
+║ _anyone otherwise that can access chats_
+╚════════════════════════╝
+`;
+          let session_id = await session.sendMessage(user, { text: "XLICON-MD;;;"+ c });      //SENDING 'base64' SESSION ID TO USER NUMBER
+          await session.sendMessage(user, { text: cc } , { quoted : session_id });
+          await require('child_process').exec('rm -rf auth_info_baileys')     //CLEAR 'auth_info_baileys' SO THAT NEXT TIME IT CLEARED FOR SCANNING
+          process.exit(1)   // STOPPING PROCESS AFTER GETTING SESSION ID
+        }
+        session.ev.on('creds.update', saveCreds)
+       if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) { sᴜʜᴀɪʟ_ʙᴀɪʟᴇʏs_ǫʀ(); } // IF ANY ERROR< THEN PRINT QR AGAIN
+      });
+    } catch (err) {console.log(err);await require('child_process').exec('rm -rf auth_info_baileys');process.exit(1);}
+  }
+  sᴜʜᴀɪʟ_ʙᴀɪʟᴇʏs_ǫʀ();
+}, 3000)
